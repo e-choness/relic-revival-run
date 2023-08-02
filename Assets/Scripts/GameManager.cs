f18cda739bf90d903112c1a22dd1c64484fc8a18
Echo Yin <echo_ybl@hotmail.com>
2023-08-02 01:18:07 -0400

    Performed code clean up and added main menu

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,13 +7,6 @@ public class GameManager : MonoBehaviour
     [SerializeField] List<GameObject> spawnPoints;
     [SerializeField] float timer;
     [SerializeField] float timeBetweenSpawns;
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     private void Update()
@@ -31,9 +23,9 @@ public class GameManager : MonoBehaviour
         {
             timer = 0;
             var randPointsIndex = Random.Range(0, 3);
-            var randObjectIndex = Random.Range(0, 3);
+            var randObjectIndex = Random.Range(0, spawnObject.Count);
             var spawn = Instantiate(spawnObject[randObjectIndex], spawnPoints[randPointsIndex].transform.position, Quaternion.identity);
-            var spawnComponent = spawn.GetComponent<spawnObjectScript>();
+            var spawnComponent = spawn.GetComponent<SpawnObjectScript>();
             spawnComponent.spawnType = randObjectIndex;
         }
     }
---
+++ Assets/Scripts/Player/ToolSelector.cs
@@ -0,0 +1,50 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ToolSelector : MonoBehaviour
+{
+    
+    [SerializeField] private List<GameObject> tools;
+    private PlayerInput playerInput;
+
+    private int toolIndex;
+    // Start is called before the first frame update
+
+    private void Awake()
+    {
+        playerInput = new PlayerInput();
+    }
+
+    private void OnEnable()
+    {
+        playerInput.Enable();
+        playerInput.Player.Switch.performed += HandleSelectTools;
+    }
+
+    private void OnDisable()
+    {
+        playerInput.Disable();
+        playerInput.Player.Switch.performed -= HandleSelectTools;
+    }
+
+    private void HandleSelectTools(InputAction.CallbackContext callback)
+    {
+        var inputValue = (int)callback.ReadValue<float>();
+        tools[toolIndex].SetActive(false);
+        
+        toolIndex = (toolIndex + inputValue) % tools.Count;
+        
+        // To prevent index is out of the bounds and continue to be able to select tools with reverse arrow
+        if (toolIndex < 0)
+        {
+            toolIndex += tools.Count;
+        }
+        tools[toolIndex].SetActive(true);
+    }
+
+    public int GetToolIndex()
+    {
+        return toolIndex;
+    }
+}
